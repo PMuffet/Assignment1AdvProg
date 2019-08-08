781feb9c0fe83b186f705805bc96dfbf1687186e
PMuffet <50091577+PMuffet@users.noreply.github.com>
2019-08-08 17:28:06 +1000

    Update Suite.java
    imported constructor from super

--- Suite.java
+++ Suite.java
@@ -1,4 +1,9 @@
 
 public class Suite extends Room {
 
+	public Suite(String id, int numBeds, String feature, String type, Character status, HiringRecord[] records) {
+		super(id, numBeds, feature, type, status, records);
+		// TODO Auto-generated constructor stub
+	}
+
 }
