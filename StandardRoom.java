b2065b617c271b70bddec77d018d61c6fdbf9abb
PMuffet <50091577+PMuffet@users.noreply.github.com>
2019-08-08 17:27:28 +1000

    Update StandardRoom.java
    imported constructor from super

--- StandardRoom.java
+++ StandardRoom.java
@@ -1,4 +1,9 @@
 
 public class StandardRoom extends Room {
 
+	public StandardRoom(String id, int numBeds, String feature, String type, Character status, HiringRecord[] records) {
+		super(id, numBeds, feature, type, status, records);
+		// TODO Auto-generated constructor stub
+	}
+
 }
