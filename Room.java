5f2a9501264b3341f1bcd12a4fe0de264cc7386b
PMuffet <50091577+PMuffet@users.noreply.github.com>
2019-08-08 17:26:07 +1000

    Update Room.java
    added accessors and mutators

--- Room.java
+++ Room.java
@@ -1,17 +1,85 @@
 
 public abstract class Room {
-	
+
 	private final String STANDARD = "Stardard";
 	private final String SUITE = "Suite";
-	
+
 	private final int MAX_RECORD_SIZE = 10;
-	
+
 	private String id;
-	private int numBedrooms;
+	private int numBeds;
 	private String feature;
 	private String type;
 	private Character status;
-	
+
 	private HiringRecord[] records = new HiringRecord[MAX_RECORD_SIZE];
+
+	public Room(String id, int numBeds, String feature, String type, Character status, HiringRecord[] records) {
+		this.id = id;
+		this.numBeds = numBeds;
+		this.feature = feature;
+		this.type = type;
+		this.status = status;
+		this.records = records;
+
+	}
+
 	
+	
+	
+	
+	// Accessors
+	public String getid() {
+		return id;
+	}
+
+	public int getnumBeds() {
+		return numBeds;
+
+	}
+
+	public String getfeature() {
+		return feature;
+	}
+
+	public String gettype() {
+		return type;
+	}
+
+	public Character getstatus() {
+		return status;
+	}
+
+	public HiringRecord[] getrecords() {
+		return records;
+	}
+
+
+	
+	
+	// Mutators
+
+	public void setid(String id) {
+		// logic?
+	}
+
+	public void setnumBeds(int numBeds) {
+		// logic?
+	}
+
+	public void setfeature(String feature) {
+		// logic?
+	}
+
+	public void settype(String type) {
+		// logic?
+	}
+
+	public void setstatus(String status) {
+		// logic?
+	}
+
+	public void setrecords(HiringRecord[] records) {
+		// logic?
+	}
 }
